c35e4bd1c1506bb7948db602e8f53b608ff58077
bambangrbp <bambangrbp123@gmail.com>
2022-04-12 21:00:43 +0700

    menambahkan contoh1.php' d

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan<h1>";
+		echo "Nama Saya Rhomadona Bambang Pamungkas";
+		echo "NIM 15200233";
+		echo "Kelas 15.4B.01";
+	}
+}
